31f52f182c799a5f68603cbe3375811d0bafe7fc
BaiBin <baibin@smuratec.com>
2021-08-09 19:44:45 +0800

    手持QR码生成完成在test目录下

---
+++ src/test/java/aliyunMessage/Test.java
@@ -0,0 +1,43 @@
+package aliyunMessage;
+
+public class Test {
+    private static String code;
+
+    public static void main(String[] args) {
+        String tel = "15221320441";  //输入手机号
+        getVerificationCode(tel);
+    }
+
+    /**
+     * 获取短信验证码
+     *
+     * @param tel
+     */
+    public static void getVerificationCode(String tel) {
+        String templateParam = "{\"code\":\"" + code + "\"}";
+//        String templateParam = "";
+        String result = ShortMessageUtil.getPhoneMsg(tel, ShortMessage.VerificationCode, templateParam);
+        if (result != null && result.equals("OK")) {
+            // 请求成功
+            System.out.println("获取验证码成功！！！");
+        } else {
+            //
+            System.out.println("获取验证码失败" + result);
+//            throw new ThirdPartException(result);
+        }
+    }
+
+        /**
+         * 生成6位随机数验证码
+         * @return
+         */
+        public static String vcode () {
+            StringBuilder code = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                int random = (int) (Math.random() * 10);
+                code = code.append(random);
+            }
+//            return code.toString();
+            return null;
+        }
+}
---
+++ src/test/java/qrCreate/QrCodeTest.java
@@ -0,0 +1,26 @@
+package qrCreate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class QrCodeTest {
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("D:\\资料\\R200\\奥思康\\14_白斌\\QR码(手持扫码专用)\\物料条码.txt"));//构造一个BufferedReader类来读取文件
+            String s = null;
+            int num = 0;
+            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+                ++num;
+//                System.out.println(s);
+                //生成二维码 String str1=str.substring(0, str.indexOf("?"));
+                QRCodeUtil.encode(s, "D:\\资料\\R200\\奥思康\\14_白斌\\QR码(手持扫码专用)\\中间的图片.png", "D:\\资料\\R200\\奥思康\\14_白斌\\QR码(手持扫码专用)\\生成的二维码\\"+ s.substring(0,s.indexOf("|")) + "-" +num + ".jpg", true);
+            }
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
