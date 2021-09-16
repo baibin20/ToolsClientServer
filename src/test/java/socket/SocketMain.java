a406be8946758687b03ab5a6132448794a425d6c
BaiBin <baibin@smuratec.com>
2021-09-16 12:59:10 +0800

    test目录提交

---
+++ src/test/java/socket/SocketMain.java
@@ -0,0 +1,29 @@
+package socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketMain extends Thread {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket();
+//        serverSocket.bind("",8081);
+
+    }
+
+    public void socket(){
+        try {
+
+        }catch (Exception ex){
+
+        }
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+
+    }
+}
